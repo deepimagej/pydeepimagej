67305e6739cac34eb2991f7e1b1fd967d8056282
esgomezm <100341044@alumnos.uc3m.es>
2020-09-30 10:35:24 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 setup(
   name = 'pydeepimagej',
   packages = find_packages(),
-  version = '1.0',   
+  version = '1.0.0',   
   license = 'BSD 2-Clause License',   
   description = 'Python package to export TensorFlow models as DeepImageJ bundled models',
   long_description=long_description,
@@ -14,7 +14,7 @@ setup(
   author = 'C. Garcia-Lopez-de-Haro, E. Gomez-de-Mariscal, L. Donati, M. Unser, A. Munoz-Barrutia, D. Sage.',
   author_email = 'esgomezm@pa.uc3m.com, daniel.sage@epfl.ch, mamunozb@ing.uc3m.es',
   url = 'https://deepimagej.github.io/deepimagej/',
-  download_url = 'https://github.com/deepimagej/pydeepimagej/archive/v1.0.tar.gz',
+  download_url = 'https://github.com/deepimagej/pydeepimagej/archive/v1.0.0.tar.gz',
   keywords = ['Fiji', 'ImageJ', 'DeepImageJ', 'Deep Learning', 'Image processing'],  
   python_requires='>=3.0',
   install_requires=[
