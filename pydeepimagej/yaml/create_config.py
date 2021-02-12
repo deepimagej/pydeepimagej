acc7786dbd6eb6dd14d5a6b968a0933654e38762
esgomezm <100341044@alumnos.uc3m.es>
2021-02-12 19:37:32 +0100

    bioimage specifications processing

--- pydeepimagej/yaml/create_config.py
+++ pydeepimagej/yaml/create_config.py
@@ -1,3 +1,39 @@
+"""
+DeepImageJ
+
+https://deepimagej.github.io/deepimagej/
+
+Conditions of use:
+
+DeepImageJ is an open source software (OSS): you can redistribute it and/or modify it under
+the terms of the BSD 2-Clause License.
+
+In addition, we strongly encourage you to include adequate citations and acknowledgments
+whenever you present or publish results that are based on it.
+
+DeepImageJ is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+You should have received a copy of the BSD 2-Clause License along with DeepImageJ.
+If not, see <https://opensource.org/licenses/bsd-license.php>.
+
+
+Reference:
+
+DeepImageJ: A user-friendly plugin to run deep learning models in ImageJ
+E. Gomez-de-Mariscal, C. Garcia-Lopez-de-Haro, L. Donati, M. Unser, A. Munoz-Barrutia, D. Sage.
+Submitted 2019.
+
+Bioengineering and Aerospace Engineering Department, Universidad Carlos III de Madrid, Spain
+Biomedical Imaging Group, Ecole polytechnique federale de Lausanne (EPFL), Switzerland
+
+Corresponding authors: mamunozb@ing.uc3m.es, daniel.sage@epfl.ch
+
+Copyright 2019. Universidad Carlos III, Madrid, Spain and EPFL, Lausanne, Switzerland.
+
+"""
+
+
 import os
 import numpy as np
 import urllib
