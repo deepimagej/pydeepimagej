7b13332a567c5ba249a9f96be9a3803716f1b2b0
esgomezm <100341044@alumnos.uc3m.es>
2021-04-13 12:20:07 +0200

    version number

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 setup(
     name='pydeepimagej',
     packages=find_packages(include=['pydeepimagej', 'pydeepimagej.*']),
-    version='2.1.0',
+    version='2.1.1',
     license='BSD 2-Clause License',
     description='Python package to export TensorFlow models as DeepImageJ bundled models',
     long_description=long_description,
